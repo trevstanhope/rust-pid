a76a1d28d8f601734623be725cc83625979b0c73
Trevor Stanhope <trevor@W510>
2015-02-27 01:25:16 -0500

    added median and mean

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
+#![feature(test)]
 extern crate time;
+extern crate test;
 
 #[test]
 fn it_works() {
@@ -14,4 +16,23 @@ pub fn pretty_print(e : &str) {
     else {
         println!("####-##-## ##:##:##\t{}", e);
     }
+}
+
+// mean of array
+pub fn mean(a : &[f32]) -> f32 {
+    let mut sum = 0.0;
+    let mut n = 0.0;
+    for p in a.iter() {
+        sum = sum + *p; // need to (*) dereference pointers
+        n = n + 1.0;
+    }
+    let f = sum / n;
+    return f;
+}
+
+// median of array
+pub fn median(a : &[f32]) -> f32 {
+    let l = a.len();
+    let f = a[l];
+    return f;
 }
--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,8 @@ fn it_works() {
 fn main() {
     let s = "something happened";
     pid::pretty_print(s); // uses a function from lib.rs
+    
+    let x: [f32; 5] = [1.0, 2.0, 3.0, 4.0, 5.0];
+    pid::mean(&x);
+    pid::median(&x);
 }
